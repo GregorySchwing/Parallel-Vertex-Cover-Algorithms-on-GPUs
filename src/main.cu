34eafebe6c958e132d21723e4bb9c16ea363f1e8
go2432 <go2432@wayne.edu>
2022-09-30 03:00:07 -0500

    caus khop mvc

--- src/main.cu
+++ src/main.cu
@@ -246,7 +246,7 @@ int main(int argc, char *argv[]) {
                 if (config.version == HYBRID && config.instance==PVC){
                     GlobalWorkListParameterized_global_kernel_MIS <<< numBlocks , numThreadsPerBlock >>> (stacks_d, workList_d, graph_d, counters_d, first_to_dequeue_global_d, global_memory_d, k_d, kFound_d, NODES_PER_SM_d, config.numHops);
                 } else if(config.version == HYBRID && config.instance==MVC) {
-                    GlobalWorkList_global_kernel <<< numBlocks , numThreadsPerBlock >>> (stacks_d, minimum_d, workList_d, graph_d, counters_d, first_to_dequeue_global_d, global_memory_d, NODES_PER_SM_d);
+                    GlobalWorkList_global_kernel_MIS <<< numBlocks , numThreadsPerBlock >>> (stacks_d, minimum_d, workList_d, graph_d, counters_d, first_to_dequeue_global_d, global_memory_d, NODES_PER_SM_d, config.numHops);
                 } else if(config.version == STACK_ONLY && config.instance==PVC){
                     LocalStacksParameterized_global_kernel <<< numBlocks , numThreadsPerBlock >>> (stacks_d, graph_d, global_memory_d, k_d, kFound_d, counters_d, pathCounter_d, NODES_PER_SM_d, config.startingDepth);
                 } else if(config.version == STACK_ONLY && config.instance==MVC) {
@@ -268,7 +268,7 @@ int main(int argc, char *argv[]) {
                 if (config.version == HYBRID && config.instance==PVC){
                     GlobalWorkListParameterized_shared_kernel_MIS <<< numBlocks , numThreadsPerBlock, sharedMemNeeded >>> (stacks_d, workList_d, graph_d, counters_d, first_to_dequeue_global_d, k_d, kFound_d, NODES_PER_SM_d, config.numHops);
                 } else if(config.version == HYBRID && config.instance==MVC) {
-                    GlobalWorkList_shared_kernel <<< numBlocks , numThreadsPerBlock, sharedMemNeeded >>> (stacks_d, minimum_d, workList_d, graph_d, counters_d, first_to_dequeue_global_d, NODES_PER_SM_d);
+                    GlobalWorkList_shared_kernel_MIS <<< numBlocks , numThreadsPerBlock, sharedMemNeeded >>> (stacks_d, minimum_d, workList_d, graph_d, counters_d, first_to_dequeue_global_d, NODES_PER_SM_d, config.numHops);
                 } else if(config.version == STACK_ONLY && config.instance==PVC){
                     LocalStacksParameterized_shared_kernel <<< numBlocks , numThreadsPerBlock, sharedMemNeeded >>> (stacks_d, graph_d, k_d, kFound_d, counters_d, pathCounter_d, NODES_PER_SM_d, config.startingDepth);
                 } else if(config.version == STACK_ONLY && config.instance==MVC) {
