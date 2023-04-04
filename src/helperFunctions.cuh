7f4db0b919a60be9a676846a292dde67e74a006d
greg <go2432@wayne.edu>
2023-04-04 18:01:37 -0500

    Damn

--- src/helperFunctions.cuh
+++ src/helperFunctions.cuh
@@ -337,7 +337,7 @@ __device__ void findUnmatchedNeighbor(CSRGraph graph,unsigned int startingVertex
         unsigned int end = graph.srcPtr[startingVertex + 1];
         
         printf("BID %d edgeIndex %d startingVertex %d\n", blockIdx.x,*edgeIndex, startingVertex);
-        for(; *edgeIndex < end-start; *edgeIndex++) { // Delete Neighbors of startingVertex
+        for(; *edgeIndex < end-start; (*edgeIndex)++) { // Delete Neighbors of startingVertex
             if (graph.matching[startingVertex]>-1 && !visited_s[graph.matching[startingVertex]]){
                     *neighbor = graph.matching[startingVertex];
                     printf("bid %d matched edge %d->%d \n", blockIdx.x,startingVertex, *neighbor);
