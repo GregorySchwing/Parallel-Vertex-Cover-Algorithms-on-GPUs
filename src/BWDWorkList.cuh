effa5c09265bf772eea5f77141da687e879fe22f
greg <go2432@wayne.edu>
2023-04-04 13:44:31 -0500

    start packing 2 vars into one var

--- src/BWDWorkList.cuh
+++ src/BWDWorkList.cuh
@@ -277,7 +277,7 @@ WorkList allocateWorkList(CSRGraph graph, Config config, unsigned int numBlocks)
 	int* count_d;
 	Counter * counter_d;
 	cudaMalloc((void**) &list_d, (graph.vertexNum) * sizeof(int) * workList.size);
-	cudaMalloc((void**) &listNumDeletedVertices_d, sizeof(unsigned int) * workList.size);
+	cudaMalloc((void**) &listNumDeletedVertices_d, sizeof(uint64_t) * workList.size);
 	cudaMalloc((void**) &tickets_d, sizeof(Ticket) * workList.size);
 	cudaMalloc((void**) &head_tail_d, sizeof(HT));
 	cudaMalloc((void**) &count_d, sizeof(int));
@@ -295,7 +295,7 @@ WorkList allocateWorkList(CSRGraph graph, Config config, unsigned int numBlocks)
 	counter.combined = 0;
 	cudaMemcpy(head_tail_d,&head_tail,sizeof(HT),cudaMemcpyHostToDevice);
 	cudaMemcpy((void*)list_d, graph.degree, (graph.vertexNum) * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemset((void*)&listNumDeletedVertices_d[0], 0, sizeof(unsigned int));
+	cudaMemset((void*)&listNumDeletedVertices_d[0], 0, sizeof(uint64_t));
 	cudaMemset((void*)&tickets_d[0], 0, workList.size * sizeof(Ticket));
 	cudaMemset(count_d, 0, sizeof(int));
 	cudaMemcpy(counter_d, &counter ,sizeof(Counter),cudaMemcpyHostToDevice);
@@ -303,6 +303,47 @@ WorkList allocateWorkList(CSRGraph graph, Config config, unsigned int numBlocks)
 	return workList;
 }
 
+WorkList allocateWorkList_DFS(CSRGraph graph, Config config, unsigned int numBlocks){
+	WorkList workList;
+	workList.size = config.globalListSize;
+	workList.threshold = config.globalListThreshold * workList.size;
+
+	volatile int* list_d;
+	volatile uint64_t* listNumDeletedVertices_d;
+	volatile Ticket *tickets_d;
+	HT *head_tail_d;
+	int* count_d;
+	Counter * counter_d;
+	cudaMalloc((void**) &list_d, (graph.vertexNum) * sizeof(int) * workList.size);
+	cudaMalloc((void**) &listNumDeletedVertices_d, sizeof(uint64_t) * workList.size);
+	cudaMalloc((void**) &tickets_d, sizeof(Ticket) * workList.size);
+	cudaMalloc((void**) &head_tail_d, sizeof(HT));
+	cudaMalloc((void**) &count_d, sizeof(int));
+	cudaMalloc((void**) &counter_d, sizeof(Counter));
+	
+	workList.list = list_d;
+	workList.listNumDeletedVertices = listNumDeletedVertices_d;
+	workList.tickets = tickets_d;
+	workList.head_tail = head_tail_d;
+	workList.count=count_d;
+	workList.counter = counter_d;
+
+	HT head_tail = 0x0ULL;
+	Counter counter;
+	counter.combined = 0;
+	cudaMemcpy(head_tail_d,&head_tail,sizeof(HT),cudaMemcpyHostToDevice);
+	//cudaMemcpy((void*)list_d, graph.degree, (graph.vertexNum) * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemset((void*)&list_d[0], 0, (graph.vertexNum) * sizeof(int));
+	uint32_t leastSignificantWord = 0;
+	uint32_t mostSignificantWord = 0;
+	uint64_t edgePair = (uint64_t) mostSignificantWord << 32 | leastSignificantWord;
+	cudaMemset((void*)&listNumDeletedVertices_d[0], edgePair, sizeof(uint64_t));
+	cudaMemset((void*)&tickets_d[0], 0, workList.size * sizeof(Ticket));
+	cudaMemset(count_d, 0, sizeof(int));
+	cudaMemcpy(counter_d, &counter ,sizeof(Counter),cudaMemcpyHostToDevice);
+
+	return workList;
+}
 void cudaFreeWorkList(WorkList workList){
 	cudaFree((void*)workList.list);
 	cudaFree(workList.head_tail);
