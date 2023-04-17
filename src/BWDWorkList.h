dfdccb70005a3d15b281d728b25aae2698b7c061
greg <go2432@wayne.edu>
2023-04-17 13:50:29 -0500

    add worklist.h

--- src/BWDWorkList.h
+++ src/BWDWorkList.h
@@ -1,6 +1,7 @@
 #ifndef BWDWORKLIST_H
 #define BWDWORKLIST_H
-
+#include <stdint.h>
+#include <stdbool.h>
 typedef unsigned int Ticket;
 typedef unsigned long long int HT;
 
@@ -12,8 +13,12 @@ typedef union {
 struct WorkList{
 	unsigned int size;
 	unsigned int threshold;
+    volatile bool* mutex;
     volatile int* list;
     volatile unsigned int* listNumDeletedVertices;
+	// For augmenting/contracting blossoms
+	volatile uint64_t * listBTypeEdges;
+
     volatile Ticket *tickets;
     HT *head_tail;
 	int* count;
